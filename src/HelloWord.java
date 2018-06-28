be4c9b2f8f85a09380839ce54167abb4f438050d
a1823979862 <maoyl@kingnet.com>
2018-06-28 10:36:33 +0800

    first commit

---
+++ src/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: maoyl
+ * @Date: 2018/6/28 10:24
+ * @Description:
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+}
